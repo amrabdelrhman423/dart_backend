addee7fb4b97104bacd4032ca1396d2d9fa8bb1d
amrabdelrhman423 <amrmedo432@gmail.com>
2023-08-23 14:30:09 +0300

    response without futur

--- routes/my_story/item.dart
+++ routes/my_story/item.dart
@@ -1,6 +1,6 @@
 import 'package:dart_frog/dart_frog.dart';
 
- onRequest(RequestContext context) async{
+Response onRequest(RequestContext context) {
   // TODO: implement route handler
   return Response(body: 'This is a new route!');
 }
